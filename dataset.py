c55b1c1e719199af01d988c7e209a961020cfc00
Nick Romano <nickpromano@gmail.com>
2017-09-21 12:28:47 -0400

    modify dataset.py

--- dataset.py
+++ dataset.py
@@ -5,65 +5,43 @@ import sys
 Some functions for loading a dataset and performing simple data preparation
 '''
 
-def load_dataset(filename, filetype='csv', header=True, index_first_col=False):
+def load_dataset(filename, filetype='csv',index_first_col=False):
 
-    '''
-    Loads a dataset from file
-    
-    Parameters:
-    -----------
-    filename: str
-        Name of data file
-    filetype: str
-        The type of data file (csv, tsv)
-    Returns:
-    --------
-    DataFrame
-        Dataset as pandas DataFrame
-    '''
+	'''
+	Loads a dataset from file
+
+	Parameters:
+	-----------
+	filename: str
+		Name of data file
+	filetype: str
+		The type of data file (csv, xls)
+	Returns:
+	--------
+	DataFrame
+		Dataset as pandas DataFrame
+	'''
+
+	# CSV file
+	if filetype == 'csv':
+		df = pandas.read_csv(filename,index_col = 0 if index_first_col else None)
 	
+	# TSV file	
+	if filetype == 'tsv':
+		df = pandas.read_table(filename,index_col = 0 if index_first_col else None)
 
-    in_file = open(filename)
-    data = []
-    header_row = ''
+	# XLS file
+	elif filetype == 'xls':
+		# TODO: add pandas.read_xls functionality
+		print('xls not yet implemented')
+		#exit()
+		
+	# Neither = problem
+	else:
+		print('Invalid file type')
+		#exit()
 
-    # Read the file line by line into instance structure
-    for line in in_file.readlines():
+	#convert to numeric
+	df = df.apply(pandas.to_numeric, errors='ignore')
 
-        # Skip comments
-        if not line.startswith("#"):
-            
-            # TSV file
-            if filetype == 'tsv':
-                if header:
-                    header_row = line.strip().split('\t')
-                else:
-                    raw = line.strip().split('\t')
-                    
-            # CSV file
-            elif filetype =='csv':
-                if header:
-                    header_row = line.strip().split(',')
-                else:
-                    raw = line.strip().split(',')
-            
-            # Neither = problem
-            else:
-                print('Invalid file type')
-                exit()
-
-            # Append to dataset appropriately
-            if not header:
-                data.append(raw)
-            header = False
-    
-    # Build a new dataframe of the data instance list of lists and return
-    df = pandas.DataFrame(data, columns=header_row)
-	
-	# convert to numeric
-    df = df.apply(pandas.to_numeric, errors='ignore')
-	
-	# index first column?
-    df.set_index(df.columns.values[0], inplace=True) if index_first_col else None
-	
-    return df
+	return df
